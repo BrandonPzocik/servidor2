9769a77be89ce7d33f21bce01545549b99eff8d5
Brandon Pzocik <pzocikbrandon@gmail.com>
2024-06-26 12:53:54 -0300

    se añadio las rutas post, put y delete

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 //mirewares
 app.use(express.json())
 
-//rutas get
+//RUTAS GET 
 //pagina principal 
 app.get("/", (req, res) => {
     res.send("Pagina Principal")
@@ -23,8 +23,36 @@ app.get("/productos/:id", (req, res) => {
     res.json(obtenerProducto)
 })
 
+//RUTA POST 
+//creamos un nuevo 
+app.post("/productos", (req, res) => {
+    const {id,producto, precio} = req.body 
+    const nuevoProducto = bd.push( {id:id, producto:producto, precio:precio})
+    res.json({mensaje:"producto creado con exito "})
+})
+
+//RUTA PUT 
+//actualizamos un producto 
+app.put("/producto/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const {producto, precio} = req.body 
+    const actualizar = bd.find( (producto) => producto.id === id)
+    actualizar.producto = producto 
+    actualizar.precio = precio 
+    res.json({mensaje: "producto actualizado de forma exitosa"})
+})
+
+//RUTA DELETE
+//eliminamos un producto 
+app.delete("/producto/:id",(req, res)=> {
+   const id = parseInt(req.params.id)
+   const obtenerProducto = bd.find( (producto) => producto.id === id)
+   const productoIndex = bd.indexOf(obtenerProducto)
+   const eliminarProducto = bd.splice(productoIndex, 1)
+   res.json({mensaje: "Producto eliminado con exitoooo"})
+})
 
 //puerto
 app.listen(4000, ()=> {
-    console.log("servidor corriendo ")
+    console.log("servidor  2 corriendo ")
 })
